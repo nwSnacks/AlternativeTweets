494c4f49ebf3a2981e8ffcd437fa57106d3f1113
Sherry Yuan <frostyyshadows@gmail.com>
2017-03-19 11:44:53 -0700

    get request working

--- server/server.py
+++ server/server.py
@@ -10,12 +10,13 @@ from HTMLParser import HTMLParser
 app = Flask(__name__)
 
 filtered_tweets = []
-markov_obj = {}
+markov_obj = markov.Markov("./raw_tweets_text.txt")
 html_parser = HTMLParser()
 
 app.config.update(dict(
     DATABASE=os.path.join(app.root_path, 'AlternativeTweetsLeaderboard.db')
-    ))
+))
+
 
 @app.cli.command('init')
 def init():
@@ -28,12 +29,14 @@ def init():
         g.cur_id = 1
     print "Initialized the server"
 
+
 def init_db():
     print "Initializing database"
     db = get_db()
     with app.open_resource('schema.sql', mode='r') as schema:
         db.cursor().executescript(schema.read())
     db.commit()
+
 
 def init_tweets():
     print "Initializing tweets"
@@ -49,25 +52,30 @@ def init_tweets():
     raw_tweets_text.close()
     print random_real_tweet()
 
+
 def init_fake_tweets():
     global markov_obj
     markov_obj = markov.Markov("./raw_tweets_text.txt")
     print random_fake_tweet()
+
 
 def connect_db():
     rv = sqlite3.connect(app.config['DATABASE'])
     rv.row_factory = sqlite3.Row
     return rv
 
+
 def get_db():
     if not hasattr(g, 'sqlite_db'):
         g.sqlite_db = connect_db()
         return g.sqlite_db
 
+
 @app.teardown_appcontext
 def close_db(error):
     if hasattr(g, 'sqlite_db'):
         g.sqlite_db.close()
+
 
 @app.route('/leaderboard', methods=['GET', 'POST'])
 def leaderboard():
@@ -75,7 +83,7 @@ def leaderboard():
         db = get_db()
         db.execute('''insert into scores (pub_date, username, score)
             values (?, ?, ?)''',
-            (int(time.time()), request.form['username'], request.form['score']))
+                   (int(time.time()), request.form['username'], request.form['score']))
         db.commit()
         return redirect(url_for('leaderboard'))
     if request.method == 'GET':
@@ -83,11 +91,12 @@ def leaderboard():
         cur = db.execute('select username, score from scores order by score desc')
         scores = cur.fetchall()
         entries = []
-        for i in range (0, 9):
+        for i in range(0, 9):
             if scores:
                 entries = scores.pop()
                 print entries[i]
         return render_template('leaderboard.html', entries=entries)
+
 
 '''
 The returned tweet will be a JSON object in this format format
@@ -96,6 +105,8 @@ The returned tweet will be a JSON object in this format format
     "true_or_false":"true"
 }
 '''
+
+
 @app.route('/question', methods=['GET'])
 def get_tweet():
     if random.randint(0, 1) == 1:
@@ -104,6 +115,17 @@ def get_tweet():
         return random_fake_tweet()
 
 def random_real_tweet():
+    raw_tweets_text = open("raw_tweets_text.txt", "w")
+    for i in range(2011, 2018):
+        with open("static/condensed_%d.json" % i) as raw_tweets_json:
+            raw_tweets = json.load(raw_tweets_json)
+            for tweet in raw_tweets:
+                if not (tweet["is_retweet"] or tweet["text"][0] == "@"):
+                    raw_tweets_text.write(
+                        html_parser.unescape(tweet["text"].encode("ascii", "ignore")).replace(".@", " @") + " ")
+                    filtered_tweets.append(tweet)
+    raw_tweets_text.close()
+
     random_tweet_index = random.randint(0, len(filtered_tweets) - 1)
     data = {}
     data["tweet"] = html_parser.unescape(
@@ -111,6 +133,7 @@ def random_real_tweet():
     data["true_or_false"] = "true"
     json_data = json.dumps(data)
     return json_data
+
 
 def random_fake_tweet():
     data = {}
